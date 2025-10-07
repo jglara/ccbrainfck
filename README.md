2ed4c7c1b064bb6008eeac7809620d57a992d544
jglara <jglara@gmail.com>
2025-10-07 07:59:05 +0200

    Step One

---
+++ README.md
@@ -0,0 +1,6 @@
+# Coding challenges
+
+This is another coding challenge for a brainfuck interpreter. See https://codingchallenges.fyi/challenges/challenge-brainfuck
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,33 @@
 #include "ccbf.hpp"
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <iterator>
+#include <ranges>
+#include <string>
 
-int main(int argc, char *argv[]) {
-    std::cout << "Brainfck\n";
-    return 0;
-}    
+namespace rng = std::ranges;
+
+int main(int argc, char* argv[]) {
+  if (argc == 1) {
+    std::string input;
+    do {
+      input.clear();
+      std::cout << "\nCCBF> ";
+      std::cin >> input;
+      std::cout << input;
+    } while (!input.empty());
+  } else {
+    std::ifstream ifs{argv[1], std::ios::in};
+    if (!ifs.is_open()) {
+      std::cerr << "Failed to open file: " << argv[1] << '\n';
+      return 1;
+    }
+
+    auto range = rng::subrange(std::istreambuf_iterator<char>{ifs},
+                               std::istreambuf_iterator<char>{});
+    rng::copy(range, std::ostream_iterator<char>{std::cout});
+  }
+
+  return 0;
+}
