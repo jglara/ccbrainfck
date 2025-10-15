49af53704050f5d1c856e8db6aa5e9421795ec0c
jglara <jglara@gmail.com>
2025-10-15 10:33:29 +0200

    Refactor compile

--- include/bfcompiler.hpp
+++ include/bfcompiler.hpp
@@ -12,46 +12,39 @@ namespace vws = std::ranges::views;
 void compile(rng::random_access_range auto const& program, std::output_iterator<inst_t> auto compiled) {
   static constexpr std::string_view valid{"<>+-,.[]"};
 
-  std::vector<char> filtered_program;
-  filtered_program.reserve(rng::size(program));
-  for (auto const instruction : program) {
-    if (valid.find(instruction) != std::string_view::npos) {
-      filtered_program.push_back(instruction);
-    }
-  }
-
+  auto filter_program_view = program | vws::filter([](auto const &c) { return valid.find(c) != std::string_view::npos; });
+  std::vector<char> filtered_program{filter_program_view.begin(), filter_program_view.end()};
+  
   auto const jumps = build_jump_table(filtered_program);
 
-  for (std::size_t pc = 0; pc < filtered_program.size(); ++pc) {
-    auto const input = filtered_program[pc];
+  auto compile_program =  vws::zip_transform([&jumps](const auto &pc, const auto &input) {
     switch (input) {
-      case '>':
-        *compiled++ = inst_t{inst_t::op_code_t::mpadd, 1};
-        break;
-      case '<':
-        *compiled++ = inst_t{inst_t::op_code_t::mpadd, -1};
-        break;
-      case '+':
-        *compiled++ = inst_t{inst_t::op_code_t::add, 1};
-        break;
-      case '-':
-        *compiled++ = inst_t{inst_t::op_code_t::add, -1};
-        break;
-      case '.':
-        *compiled++ = inst_t{inst_t::op_code_t::out, 0};
-        break;
-      case ',':
-        *compiled++ = inst_t{inst_t::op_code_t::in, 0};
-        break;
-      case '[':
-        *compiled++ = inst_t{inst_t::op_code_t::jmpz, static_cast<std::int32_t>(jumps[pc])};
-        break;
-      case ']':
-        *compiled++ = inst_t{inst_t::op_code_t::jmpnz, static_cast<std::int32_t>(jumps[pc])};
-        break;
-      default:
-        break;
+    case '>':
+      return inst_t{inst_t::op_code_t::mpadd, 1};
+    case '<':
+      return inst_t{inst_t::op_code_t::mpadd, -1};
+      
+    case '+':
+      return inst_t{inst_t::op_code_t::add, 1};
+      
+    case '-':
+      return inst_t{inst_t::op_code_t::add, -1};
+      
+    case '.':
+      return inst_t{inst_t::op_code_t::out, 0};
+      
+    case ',':
+      return inst_t{inst_t::op_code_t::in, 0};
+      
+    case '[':
+      return inst_t{inst_t::op_code_t::jmpz, static_cast<std::int32_t>(jumps[pc])};
+      
+    case ']':
+      return inst_t{inst_t::op_code_t::jmpnz, static_cast<std::int32_t>(jumps[pc])};
     }
-  }
+    return inst_t{inst_t::op_code_t::nop, 0}; // should not happen
+  }, vws::iota(0), filtered_program);
+
+  rng::copy(compile_program, compiled);
 }
 
--- include/bytecode.hpp
+++ include/bytecode.hpp
@@ -3,12 +3,13 @@
 
 struct inst_t {
   enum class op_code_t : std::uint8_t {
+    nop,
     mpadd,  // add a value to memory pointer
     add,    // add a value to the memory
     jmpz,    // jump to location if [mem] == 0
-    jmpnz,
-    in,
-    out,
+    jmpnz, // jump to location if [mem] != 0
+    in, // input 1 char at [mem]
+    out, // output 1 char at [mem]
   };
 
   op_code_t opcode;
